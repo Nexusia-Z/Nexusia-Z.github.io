e3d0e54cf92799acd149b20d7db6ee53929a93e1
DitherWither <demonslayervardhan@gmail.com>
2023-05-24 12:49:43 +0530

    change font and color pallete

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,18 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				mono: ["Space Mono", ...defaultTheme.fontFamily.mono],
+				sans: ["InterVariable", "Inter", ...defaultTheme.fontFamily.sans],
+
+				display: ["Space Mono", ...defaultTheme.fontFamily.mono],
+				body: ["InterVariable", ...defaultTheme.fontFamily.sans],
+			}
+		},
 	},
 	plugins: [],
 }
